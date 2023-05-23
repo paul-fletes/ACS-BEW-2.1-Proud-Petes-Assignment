8a032e7846c4ad1970de0729b4b86797ea15b28d
Paul Fletes <paul.fletes@gmail.com>
2023-05-23 12:13:30 -0500

    cloned starter and add comments

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 if (!process.env.PORT) {
-  require('dotenv').config()
+  require('dotenv').config() // load .env file if not in production
   process.env.NODE_ENV = "dev"
 }
 
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override')
+const favicon = require('serve-favicon'); // serves favicon.ico file located in /public
+const logger = require('morgan'); // logs HTTP requests to the console
+const cookieParser = require('cookie-parser'); // parses cookies attached to incoming requests to make available in `req.cookies`
+const bodyParser = require('body-parser'); // parses HTTP request body. Available under the `req.body` property
+const methodOverride = require('method-override') // allows us to use HTTP verbs such as PUT or DELETE in places where the client doesn't support it
 
 const app = express();
 
@@ -21,10 +21,11 @@ mongoose.connect('mongodb://localhost/local', {
   useFindAndModify: false
 });
 
-// view engine setup
+// view engine setup to use Pug templates located in /views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// serves static files located in /public
 app.use(express.static(path.join(__dirname, 'public')));
 
 // override with POST having ?_method=DELETE or ?_method=PUT
